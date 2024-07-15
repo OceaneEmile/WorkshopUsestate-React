689c8ff9f25401eb0c6920971597abd3dce47814
oceaneEmile <oceane.emile@oclock.school>
2024-07-15 11:40:16 +0200

    ajout du copy image on clipboard

--- my-app/app/page.jsx
+++ my-app/app/page.jsx
@@ -5,14 +5,48 @@ import { renderPNG } from "./render-png";
 import Link from "next/link";
 
 export default function Home() {
-
   const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState("idle");
 
   const [settings, setSettings] = useState({
     padding: 16, // ce sont les valeurs par défaut
     shadow: 10,
     radius: 16,
   });
+
+  // Méthode pour désactiver le bouton download si telechargement en cours
+  const handleDownload = async (isCopying) => {
+    setLoading(isCopying ? "copying" : "downloading");
+    const { blob } = await renderPNG({
+      image,
+      settings,
+    });
+    const url = URL.createObjectURL(blob);
+
+    if (isCopying) {
+      try {
+        await navigator.clipboard.write([
+          new ClipboardItem({
+            "image/png": blob,
+          }),
+        ]);
+        alert("Image copiée dans le presse-papiers !");
+      } catch (err) {
+        console.error("Échec de la copie de l'image : ", err);
+        alert("La copie de l'image a échoué.");
+      }
+    } else {
+      const link = document.createElement("a");
+      link.download = image.name.replace(/\.(jpg|jpeg|png)$/i, "-elevation.png");
+      link.href = url;
+      document.body.appendChild(link); // Ajout temporaire à l'élément body
+      link.click();
+      document.body.removeChild(link); // Suppression après le clic
+      URL.revokeObjectURL(url); // Libération de l'URL
+    }
+    setLoading("idle");
+  }
+
   // Méthode pour modifier les paramètres
   const setSetting = (name, value) => {
     setSettings((curr) => ({
@@ -20,14 +54,17 @@ export default function Home() {
       [name]: value, // il faut utiliser la syntaxe array pour pouvoir modifier les valeurs
     }));
   };
+
   // Méthode pour gérer l'upload d'image
   const handleImageUpload = (event) => {
-    // const file = event.target.files[0];
-    // setImage(URL.createObjectURL(file));
-    // console.log(file);
     const files = event.target.files;
     const file = files[0];
-    console.log(file);
+
+    // Vérification du type de fichier avec message d'erreur
+    if (file.type !== "image/jpeg" && file.type !== "image/png") {
+      alert("Veuillez choisir une image au format JPEG ou PNG");
+      return;
+    }
 
     const reader = new FileReader();
 
@@ -37,7 +74,7 @@ export default function Home() {
       img.onload = function () {
         setImage({
           width: img.width,
-          height : img.height,
+          height: img.height,
           src: img.src,
           name: file.name,
         });
@@ -46,16 +83,13 @@ export default function Home() {
     };
     reader.readAsDataURL(file);
   };
-//console.log(image);
-console.log(settings);
 
   return (
     <main className="flex justify-center items-center m-auto max-w-4xl max-lg:flex-col gap-8 min-h-full">
-      <div class="card bg-base-100 w-96 shadow-xl">
-      <div className="w-full h-fit border rounded-md">
-      <ImageGenerator settings={settings} image={image} />
-      </div>
-
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="w-full h-fit border rounded-md">
+          <ImageGenerator settings={settings} image={image} />
+        </div>
       </div>
       <div className="card-body">
         <span>Files</span>
@@ -70,7 +104,7 @@ console.log(settings);
           min="0"
           max="100"
           value={settings.padding}
-          onChange={(e) => setSetting("padding", Number (e.target.value))} // on change la valeur de padding : onChange={(e) => setSetting("key", e.target.value)}
+          onChange={(e) => setSetting("padding", Number(e.target.value))} // on change la valeur de padding : onChange={(e) => setSetting("key", e.target.value)}
           className="range range-accent"
         />
         <span>Shadow</span>
@@ -78,8 +112,8 @@ console.log(settings);
           type="range"
           min="0"
           max="100"
-           value={settings.shadow}
-           onChange={(e) => setSetting("shadow",  Number (e.target.value))}
+          value={settings.shadow}
+          onChange={(e) => setSetting("shadow", Number(e.target.value))}
           className="range range-accent"
         />
         <span>Radius</span>
@@ -88,29 +122,24 @@ console.log(settings);
           min="0"
           max="100"
           value={settings.radius}
-          onChange={(e) => setSetting("radius",  Number (e.target.value))}
+          onChange={(e) => setSetting("radius", Number(e.target.value))}
           className="range range-accent"
         />
       </div>
-          <button
-      className="btn"
-      onClick={async () => {
-        const { blob } = await renderPNG({
-          image,
-          settings,
-        });
-        //console.log(blob);
-        const url = URL.createObjectURL(blob);
-
-        const link = document.createElement("a");
-        link.download = image.name.replace(".jpg","-elevation.png");
-        link.href = url;
-        link.click();
-      }}
-    >
-      Download
-    </button>
-
+      <button
+        className="btn"
+        onClick={() => handleDownload(false)}
+        disabled={loading !== "idle"}
+      >
+        Download {loading === "downloading" ? "..." : ""}
+      </button>
+      <button
+        className="btn-primary"
+        onClick={() => handleDownload(true)}
+        disabled={loading !== "idle"}
+      >
+        Copy {loading === "copying" ? "..." : ""}
+      </button>
     </main>
   );
 }
